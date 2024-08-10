63c002d1e57cba222f514b19b08ba031b067c75c
Rohitdandge <rohitpatil143d@gmail.com>
2024-08-10 20:50:04 +0530

    basic c program

---
+++ king.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+int r =6;
+int height =4;
+printf("the area of circle with radius %d is %f\n",r,3.14*r*r);
+printf("the volume of cylinder with radius %d is and Height %d is :%f", r,height,3.14*r*r*height);
+
+
+
+
+return 0;
+
+
+
+
+
+
+
+
+
+
+}
